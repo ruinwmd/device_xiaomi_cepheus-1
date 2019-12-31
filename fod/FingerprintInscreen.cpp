74f61081caa973aa24839ca69ab1d959ef6f58d6
Demon000 <demonsingur@gmail.com>
2019-12-31 01:06:45 +0100

    Revert "cepheus: fod: Improve semantics"
    This reverts commit 6ba4b5df97592f09afc69ad7f787fb6cb10600bc.

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -129,7 +129,7 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
     return Void();
 }
 
-Return<int32_t> FingerprintInscreen::getDimAmount(int32_t /* brightness */) {
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
     int realBrightness = get(BRIGHTNESS_PATH, 0);
     float alpha;
 
@@ -139,6 +139,8 @@ Return<int32_t> FingerprintInscreen::getDimAmount(int32_t /* brightness */) {
         alpha = 1.0 - pow(realBrightness / 1680.0, 0.455);
     }
 
+    (void) brightness;
+
     return 255 * alpha;
 }
 
@@ -146,7 +148,8 @@ Return<bool> FingerprintInscreen::shouldBoostBrightness() {
     return false;
 }
 
-Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>& /* callback */) {
+Return<void> FingerprintInscreen::setCallback(const sp<::vendor::pa::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreenCallback>& callback) {
+    (void) callback;
     return Void();
 }
 
