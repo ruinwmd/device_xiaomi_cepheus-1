0b16c67e3129a96d2538e088a11d10082f33a1a8
DarkDampSquib <andrin.geiger1998@gmail.com>
2020-02-08 12:45:31 +0100

    cepheus: Use Walleye FP for SafetyNet
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- init/init_cepheus.cpp
+++ init/init_cepheus.cpp
@@ -57,5 +57,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "cepheus-user 10 QKQ1.190825.002 V11.0.7.0 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys");
 }
